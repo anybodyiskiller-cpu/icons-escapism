9bc9e43cb0aa1a5312150531c96d67a60572277a
ᴇsᴄᴀᴘɪsᴍ <anybodyiskiller@Outlook.com>
2026-03-30 17:19:21 +0800

    Create generate.js

---
+++ generate.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+const ICON_DIR = "./icons";
+const OUTPUT = "./icons.json";
+
+function getIcons() {
+  if (!fs.existsSync(ICON_DIR)) return [];
+
+  const files = fs.readdirSync(ICON_DIR);
+
+  return files
+    .filter(file => file.endsWith(".png"))
+    .map(file => {
+      const name = path.basename(file, ".png");
+      return {
+        name,
+        file,
+        path: `icons/${file}`
+      };
+    });
+}
+
+const icons = getIcons();
+
+fs.writeFileSync(OUTPUT, JSON.stringify(icons, null, 2));
+
+console.log("icons.json 已生成");
